98c49ee9cfef2213719bc60cd440c622c9206f28
MatanShohat <matan.shohat@gmail.com>
2019-05-14 23:48:40 +0300

    start building algorithm

--- ORAM.sv
+++ ORAM.sv
@@ -8,6 +8,8 @@ module oram_module(
 	input [(8*a)-1:0] w_value, // only in write operation, this input holds the value which will be written to block number rw_block_number
 	input rw_indicator, // indicates wheter it is a read or write operation (rw_indicator=0 - read, rw_indicator=1 - write)
 	input input_ready, // indicates there are valid inputs on the line
+	input clk, // core's clock
+	input rst, // reset signal, active high
 	output [(8*a)-1:0] r_value, // only in read operation, this output holds the value of block number rw_block_number
 	output output_ready); // indicates there are valid outputs on the line
 	
@@ -32,6 +34,18 @@ module oram_module(
 		$display ("oram module has been successfully created");
 	end
 	
-	
+	always@(posedge clk iff (input_ready == 1 and rst == 0) or posedge rst) begin // enter only in posedge clk only if input is ready, asynchronous rst signal
+		if (rst) begin // reset output signals
+			r_value <= 0;
+			output_ready <= 0;
+		end else begin // input is ready, do operation
+			if (rw_indicator == 0) begin // oread operation
+				// implement oread
+			end else begin // owrite operation
+				// implement owrite
+			end
+		end
+		
+	end
 
 endmodule
